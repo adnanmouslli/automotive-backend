55108b51f91ee7a161d077c4a206e803c1b5ec61
Adnan Mouslli <yazanmouslli6@gmail.com>
2026-02-21 23:16:41 +0300

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as fs from 'fs';
 import * as express from 'express';
-
+// test
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
